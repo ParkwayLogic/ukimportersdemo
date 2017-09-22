f515c5ba03fda9c115a0958cf490580edd3288cb
raminty <rtmailings@sent.com>
2017-09-22 11:03:32 +0100

    Replace with code that includes flask

--- find_competitors.py
+++ find_competitors.py
@@ -1,8 +1,19 @@
 """
 Return exporters with overlapping most frequent commodity codes
 """
+import sys
+sys.path.insert(0,'lib')
 import csv, utils
 import networkx as nx
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.externals import joblib
+from flask import Flask, request, jsonify
+#from flask_restplus import Resource, Api #, reqparse
+
+app = Flask(__name__)
+#api = Api(app)
 
 def get_top_nodes(Gph, company):
     """
@@ -47,22 +58,34 @@ def find_common_codes(Gph, common_HS):
             yield cmdty_node
 
 
-def main(*args):
+def HS_to_predicted_SIC(HScode):
+    """Return SIC code (with verbal description) against an HS code"""
+    clf = joblib.load('model_1_trained_classifier_100000_1.pkl')
+    prediction = clf.predict(HScode)
+    return prediction
+
+
+#@api.route('/<str:args>')
+@app.route('/', methods=['GET'])
+def main():
     """Runs the main program"""
-    rg = []
-    for i, r in enumerate(*args):
-        rg.append(r)
+    rg = dict(request.args.items())
+    print('rg =', rg)
     print('Loading main graph ...')
     sourcedata='Export_combined_summary.csv'
     Gph = load_data(sourcedata)
 
-    if len(rg)==4:  # given two commodity codes
-        common_HS = [rg[2], rg[3]]
-    elif len(rg)==3:  # given a company name
-        tops = [t for t in get_top_nodes(Gph, rg[2])]
+    if ('cn1' in rg) and ('cn2' in rg):  # given two commodity codes
+        common_HS = [rg['cn1'], rg['cn2']]
+    elif 'name' in rg:  # given a company name
+        tops = [t for t in get_top_nodes(Gph, rg['name'])]
         if len(tops) > 1:
             top1, top2 = tops[:2]
         else:
+            """
+            1. Look up SIC code from company name
+            2. Lookup top two HS codes for SIC code, from table to be created
+            """
             print('SIC-based search not implemented')
             raise NotImplementedError
         common_HS = [top1[1], top2[1]]
@@ -71,43 +94,58 @@ def main(*args):
         common_HS=['94033019','94034090']
     
     print('Identified', common_HS[0], common_HS[1])
-    c1 = utils.get_desc_by_CN(common_HS[0])
-    c2 = utils.get_desc_by_CN(common_HS[1])
-    print('Preparing subgraph for\n{0} - {1}\nand\n{2} - {3}'.format(
-        common_HS[0], c1['Self-Explanatory text (English)'].values[0],
-        common_HS[1], c2['Self-Explanatory text (English)'].values[0]
-    ))
-    nodes1 = Gph.subgraph(find_common_codes(Gph, common_HS))
-    nx.write_gexf(nodes1,'common_subgraph.gexf')
+    # c1 = utils.get_desc_by_CN(common_HS[0])
+    # c2 = utils.get_desc_by_CN(common_HS[1])
+    # print('Preparing subgraph for\n{0} - {1}\nand\n{2} - {3}'.format(
+    #     common_HS[0], c1['Self-Explanatory text (English)'].values[0],
+    #     common_HS[1], c2['Self-Explanatory text (English)'].values[0]
+    # ))
+    # nodes1 = Gph.subgraph(find_common_codes(Gph, common_HS))
+    # nx.write_gexf(nodes1,'common_subgraph.gexf')
     # Generate text response
-    names = [n for n in nx.common_neighbors(Gph, common_HS[0], common_HS[1])]
-    s = ' '.join([
-        'You have {0} companies in your neighbourhood'
-    ])
-    print(s.format(len(names)-1))
+    # names = [n for n in nx.common_neighbors(Gph, common_HS[0], common_HS[1])]
+    # s = ' '.join([
+    #     'You have {0} companies in your neighbourhood'
+    # ])
+    # print(s.format(len(names)-1))
     i = 0
     for name in names:
         cmdties = [c for c in get_top_nodes(Gph, name)]
-        is_interesting = True
-        if len(names) > 20:
-            # print((common_HS[0] in cmdties[1]), (common_HS[1] in cmdties[1]))
-            if (common_HS[0] in cmdties[1]) or (common_HS[1] in cmdties[1]):
-                is_interesting = True
-            else:
-                is_interesting = False
-        if (len(cmdties) > 2) and is_interesting and i < 20:
-            i += 1
-            s = ' '.join([
-                '{0} has exported {1} different commodities'
-            ])
-            print(s.format(name, len(cmdties)))
-    
+        # is_interesting = True
+        # if len(names) > 20:
+        #     # print((common_HS[0] in cmdties[1]), (common_HS[1] in cmdties[1]))
+        #     if (common_HS[0] in cmdties[1]) or (common_HS[1] in cmdties[1]):
+        #         is_interesting = True
+        #     else:
+        #         is_interesting = False
+        # if (len(cmdties) > 2) and is_interesting and i < 20:
+        #     i += 1
+        #     s = ' '.join([
+        #         '{0} has exported {1} different commodities'
+        #     ])
+        # print(s.format(name, len(cmdties)))
+    retdict = {}
+    for name in names:
+        retvals = dict([(c[1], c[2]) for c in get_top_nodes(Gph, name)])
+        retdict[name] = retvals
+    #print(retdict)
+    return jsonify({'competitors': dict(retdict)})
+
+
+@app.errorhandler(500)
+def server_error(e):
+   #logging.exception('An error occurred during a request.')
+   return """
+   An internal error occurred: <pre>{}</pre>
+   See logs for full stacktrace.
+   """.format(e), 500
+
 
 if __name__ == '__main__':
-    import sys
-    main(sys.argv)
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    # import sys
+    #main(sys.argv)
 
 # 84118280 84119900
 # 94033019 94034090
 # 82121090 84191100
-
